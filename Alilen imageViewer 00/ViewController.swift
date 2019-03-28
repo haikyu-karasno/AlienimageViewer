a74ab97dcd0e6760a579124780da096bb087c108
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-03-28 14:51:45 +0900

    Initial Commit

---
+++ Alilen imageViewer 00/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alilen imageViewer 00
+//
+//  Created by D7703_16 on 2019. 3. 28..
+//  Copyright © 2019년 201526109. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
