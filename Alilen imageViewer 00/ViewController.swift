cc41004ea3e07b5ae6f767c01202cefb3ad03bf7
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-03-28 15:30:07 +0900

    이건 이미지

--- Alilen imageViewer 00/ViewController.swift
+++ Alilen imageViewer 00/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienImageviewer: UIImageView!
+    @IBOutlet weak var countLabal: UILabel!
+    
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       alienImageviewer.image =  UIImage(named: "frame1.png")
+        countLabal.text = "frame\"(count).png"
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        count+=1
+        
+        if(count>5){
+            count = 1
+        }
+        
+        alienImageviewer.image = UIImage(named: "frame\(count).png")
+        countLabal.text = "frame\(count).png"
+        print("Button pressed!")
+    }
+    
 }
+
 
